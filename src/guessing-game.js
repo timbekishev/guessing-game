6bff52f821d3d10f68d7fdad22c48b450cc69780
Timofei <btv.97@bk.ru>
2017-01-28 18:17:04 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,7 +2,7 @@ class GuessingGame {
     constructor() {}
         this.min=0;
         this.max=0;
-        this.number=0;
+        this.num=0;
     setRange(min, max) {
         this.min=0;
         this.max=10;
